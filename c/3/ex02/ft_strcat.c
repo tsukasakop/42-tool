ecd14ee017b9f896f03ff93b08b60f89c1d6898c
tsukasakop <tsukasakop@gmail.com>
2024-01-19 03:35:33 +0900

    C: implement ft_strcat [C03EX02]

---
+++ c/3/ex02/ft_strcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 02:48:31 by tkondo            #+#    #+#             */
+/*   Updated: 2024/01/19 03:03:33 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char*src)
+{
+	char*	ptr;
+
+	ptr = dest;
+	while(*ptr != '\0')
+		ptr++;
+	while(*src != '\0'){
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return(dest);
+}
+/*
+#include <stdio.h>
+int main() {
+    char dest[20] = "Hello";
+    char src[] = ", World!";
+
+    ft_strcat(dest, src);
+
+    printf("Concatenated string: %s\n", dest);
+
+    return 0;
+}
+//*/
