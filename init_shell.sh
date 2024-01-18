42bd0748f0883efbf68dc40da3798b8063f4277a
tsukasakop <tsukasakop@gmail.com>
2024-01-18 20:18:18 +0900

    C: create shell initialize script

---
+++ init_shell.sh
@@ -0,0 +1,16 @@
+# Environment Variales
+export FT_HOME=`realpath $0 | xargs dirname`
+
+# Aliases
+alias 42home='cd '$FT_HOME
+#alias 42work
+#alias 42rev
+#alias 42urlopen
+#alias 42urladd
+#alias 42gc
+#alias 
+alias 42cc='cc -Wall -Wextra -Werror -fsanitize=address'
+alias 42norm='norminette -R CheckForbiddenSourceHeader'
+
+alias ll='ls -ltrG'
+alias lr='ls -lR'
