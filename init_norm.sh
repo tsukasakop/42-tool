c88c401357a44a3851814ca746d49b04bc48246b
tsukasakop <tsukasakop@gmail.com>
2024-01-19 05:54:29 +0900

    A: add installation for 42 formatter for vim

--- init_norm.sh
+++ init_norm.sh
@@ -1,2 +1,8 @@
+# install norminette
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install norminette==3.3.51
+
+# install vim formatter
+python3 -m pip install c_formatter_42
+git clone https://github.com/cacharle/c_formatter_42.vim
+./c_formatter_42.vim/install
