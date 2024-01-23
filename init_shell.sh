9d57f860c7934231a3d413951432f8ad4ec19b26
tsukasakop <tsukasakop@gmail.com>
2024-01-24 08:21:55 +0900

    M: removed FT_HOME env arg

--- init_shell.sh
+++ init_shell.sh
@@ -1,16 +1,16 @@
 # Environment Variales
-export FT_HOME=`realpath $0 | xargs dirname`
+# export FT_HOME=`realpath $0 | xargs dirname`
 
 # Aliases
 alias 42home='cd '$FT_HOME
 alias 42url=$FT_HOME'/open_url.sh'
-#alias 42work
+# alias 42work
 
-#alias 42gc
+# alias 42gc
 
 alias 42cc='cc -Wall -Wextra -Werror -fsanitize=address'
 
-#error handring
+# error handring
 alias 42norm='norminette -R CheckForbiddenSourceHeader'
 
 alias ll='ls -ltarG'
